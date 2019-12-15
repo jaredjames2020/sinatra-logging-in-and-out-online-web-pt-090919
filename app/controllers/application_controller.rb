8f7d86ff32e5579fec627d501f6faa99e8cff049
Jared S <jaredjames2020@gmail.com>
2019-12-15 20:16:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < Sinatra::Base
       session[:user_id] = @user.id
       redirect '/account'
     end
-      binding.pry
+      # binding.pry
       erb :error
   end
 
